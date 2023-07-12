d0275da6c316c0e8b801353544b7dc04885a6843
evgenia <kordeugenie@yandex.ru>
2023-07-12 15:54:06 +0500

    get ingredient

--- src/product/router.py
+++ src/product/router.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 import jwt
 
 from src.database import get_db
-from src.models.models import Product
+from src.models.models import Product, IngredientsProduct
 
 router = APIRouter(
     prefix="/product",
@@ -20,4 +20,10 @@ def product_by_id(id_product: int, session: Session = Depends(get_db)):
 @router.get("/products")
 def all_product(session: Session = Depends(get_db)):
     stmt = session.query(Product).all()
+    return stmt
+
+
+@router.get("/ingredient")
+def ingredient_by_id_product(id_product: int, session: Session = Depends(get_db)):
+    stmt = session.query(IngredientsProduct).filter_by(id_product=id_product).all()
     return stmt
